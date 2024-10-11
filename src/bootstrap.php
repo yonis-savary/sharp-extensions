210f1cc7ae87958a8d76da23fc94da24c3c41741
yonis-savary <yonissavary@outlook.fr>
2024-10-11 11:50:38 +0200

    Updated extensions bootstrap

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,11 +1,15 @@
 <?php
 
+use YonisSavary\Sharp\Classes\Core\EventListener;
+use YonisSavary\Sharp\Classes\Events\LoadingFramework;
 use YonisSavary\Sharp\Core\Autoloader;
 
-foreach (scandir(__DIR__) as $directory)
-{
-    if ($directory == "." || $directory == ".." || $directory == "bootstrap.php")
-        continue;
+EventListener::getInstance()->on(LoadingFramework::class, function(){
+    foreach (scandir(__DIR__) as $directory)
+    {
+        if ($directory == "." || $directory == ".." || $directory == "bootstrap.php")
+            continue;
 
-    Autoloader::loadApplication( __DIR__ . "/$directory");
-}
+        Autoloader::loadApplication( __DIR__ . "/$directory");
+    }
+});
