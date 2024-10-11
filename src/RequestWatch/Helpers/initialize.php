5f03e48af6a4853cf7fccbce4b9d296873c485bd
yonis-savary <yonissavary@outlook.fr>
2024-10-11 23:15:13 +0200

    Added Config Check to request-watch

--- src/RequestWatch/Helpers/initialize.php
+++ src/RequestWatch/Helpers/initialize.php
@@ -5,9 +5,12 @@ use YonisSavary\Sharp\Classes\Events\RoutedRequest;
 use YonisSavary\Sharp\Classes\Http\Request;
 use YonisSavary\Sharp\Extensions\RequestWatch\Classes\RequestWatch;
 
-$watch = RequestWatch::getInstance();
-$watch->registerRequest(Request::buildFromGlobals());
+if ((config("request-watcher", ["enabled" => false])["enabled"] ?? false) === true )
+{
+    $watch = RequestWatch::getInstance();
+    $watch->registerRequest(Request::buildFromGlobals());
 
-EventListener::getInstance()->on(RoutedRequest::class, function(RoutedRequest $event) use (&$watch){
-    $watch->registerRoute($event->route);
-});
+    EventListener::getInstance()->on(RoutedRequest::class, function(RoutedRequest $event) use (&$watch){
+        $watch->registerRoute($event->route);
+    });
+}
