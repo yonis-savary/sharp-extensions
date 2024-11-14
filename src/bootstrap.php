95c3cd73b4a925b45b7623dd5a1b7aa616da1ab9
yonis-savary <yonissavary@outlook.fr>
2024-11-14 14:08:34 +0100

    Safer `bootstrap.php` file for non-sharp project

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -4,12 +4,15 @@ use YonisSavary\Sharp\Classes\Core\EventListener;
 use YonisSavary\Sharp\Classes\Events\LoadingFramework;
 use YonisSavary\Sharp\Core\Autoloader;
 
-EventListener::getInstance()->on(LoadingFramework::class, function(){
-    foreach (scandir(__DIR__) as $directory)
-    {
-        if ($directory == "." || $directory == ".." || $directory == "bootstrap.php")
-            continue;
+if (class_exists(EventListener::class))
+{
+    EventListener::getInstance()->on(LoadingFramework::class, function(){
+        foreach (scandir(__DIR__) as $directory)
+        {
+            if ($directory == "." || $directory == ".." || $directory == "bootstrap.php")
+                continue;
 
-        Autoloader::loadApplication( __DIR__ . "/$directory", true);
-    }
-});
+            Autoloader::loadApplication( __DIR__ . "/$directory", true);
+        }
+    });
+}
